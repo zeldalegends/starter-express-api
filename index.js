f08c0ebca7730d5a34e376c1de097c79fdd7a92f
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2023-02-27 02:44:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express')
-var bodyParser = require('body-parser')
+const express = require('express');
+var bodyParser = require('body-parser');
 
-const app = express()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/status', (req, res) => {
-    console.log("healthy")
+    console.log("healthy");
     res.send('Express App: Running (Healthy).');
 })
 
@@ -18,4 +20,7 @@ app.post('/', urlencodedParser, (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(process.env.PORT || 3000)
+app.listen(PORT, (err) => {
+    if (err) console.log("Error in server setup");
+    console.log("Server listening on port", PORT);
+});
