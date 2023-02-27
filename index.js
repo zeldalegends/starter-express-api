734a1ba6d76f57a218cdb33a1f7fbc4be54352cf
zeldalegends <73659648+zeldalegends@users.noreply.github.com>
2023-02-27 02:39:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 })
+
+app.get('/status', (req, res) => {
+    console.log("healthy")
+    res.send('Express App: Running (Healthy).');
+})
+
+app.post('/', urlencodedParser, (req, res) => {
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+});
+
 app.listen(process.env.PORT || 3000)
